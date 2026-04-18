35ea291fd19d9c1516937c317252f61435df9003
ruhsi <rushi@gmail.com>
2026-04-18 10:48:19 +0530

    Initial commit

---
+++ src/main/java/com/rm/bu/BluckUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.rm.bu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluckUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluckUpdateApplication.class, args);
+    }
+
+}
